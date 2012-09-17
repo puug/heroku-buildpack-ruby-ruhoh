58b726e61cf6e60996284a22cf7a77b31e9d35dd
puug <rmurfitt@gmail.com>
2012-09-17 22:46:46 +1000

    Update lib/language_pack/ruby.rb

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -526,10 +526,8 @@ params = CGI.parse(uri.query || "")
 
   def generate_ruhoh_site
     puts "Building ruhoh site"
-    run("env PATH=$PATH bundle exec ruhoh compile 2>&1")
-    puts "Output #{$?}"
-    unless $? == 0
-      error "Failed to generate site with ruhoh."
-    end
+    output = run_stdout("env PATH=$PATH bundle exec ruhoh compile")
+    puts "Output #{output}"
+    error "Failed to generate site with ruhoh."
   end
 end
